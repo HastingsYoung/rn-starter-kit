f160c736ab212e683e65f56b1c3af5bf9a4fdde5
Guangling.Hastings <hastings_gl@outlook.com>
2017-05-11 23:59:45 +1000

    Add Video Player.

--- index.ios.js
+++ index.ios.js
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#efefef',
+        backgroundColor: '#F5F5F5',
     }
 });
 
--- js/containers/LiveStreamView.js
+++ js/containers/LiveStreamView.js
@@ -9,6 +9,8 @@ import {
     Dimensions,
     Button
 } from 'react-native';
+import Video from 'react-native-video';
+
 const { width, height } = Dimensions.get('window');
 
 export default class LiveStreamView extends Component {
@@ -25,7 +27,28 @@ export default class LiveStreamView extends Component {
 
         return <View style={styles.container} tabLabel={this.props.tabLabel}>
             <Text>LiveStream</Text>
-            <Button title="BACK" onPress={() => this.props.tabView.goToPage(0) }/>
+            <Video source={require('../../assets/video/broadchurch.mp4')}   // Can be a URL or a local file.
+                   ref={(ref) => {
+         this.player = ref
+       }}                                      // Store reference
+                   rate={1.0}                              // 0 is paused, 1 is normal.
+                   volume={1.0}                            // 0 is muted, 1 is normal.
+                   muted={false}                           // Mutes the audio entirely.
+                   paused={false}                          // Pauses playback entirely.
+                   resizeMode="cover"                      // Fill the whole screen at aspect ratio.*
+                   repeat={true}                           // Repeat forever.
+                   playInBackground={false}                // Audio continues to play when app entering background.
+                   playWhenInactive={false}                // [iOS] Video continues to play when control or notification center are shown.
+                   ignoreSilentSwitch={"ignore"}           // [iOS] ignore | obey - When 'ignore', audio will still play with the iOS hard silent switch set to silent. When 'obey', audio will toggle with the switch. When not specified, will inherit audio settings as usual.
+                   progressUpdateInterval={250.0}          // [iOS] Interval to fire onProgress (default to ~250ms)
+                   //onLoadStart={this.loadStart}            // Callback when video starts to load
+                   //onLoad={this.setDuration}               // Callback when video loads
+                   //onProgress={this.setTime}               // Callback every ~250ms with currentTime
+                   //onEnd={this.onEnd}                      // Callback when playback finishes
+                   //onError={this.videoError}               // Callback when video cannot be loaded
+                   //onBuffer={this.onBuffer}                // Callback when remote video is buffering
+                   //onTimedMetadata={this.onTimedMetadata}  // Callback when the stream receive some metadata
+                   style={styles.backgroundVideo} />
         </View>;
     }
 }
@@ -36,5 +59,12 @@ const styles = StyleSheet.create({
         flexDirection: "column",
         justifyContent: 'center',
         alignItems: 'center',
-    }
+    },
+    backgroundVideo: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+    },
 });
